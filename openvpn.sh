669ec42b0cd7d91b4cb7435fb9f65338c0995e82
Ubuntu <ubuntu@ip-172-26-12-181.ec2.internal>
2021-03-20 05:50:48 +0000

    update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -1,3 +1,4 @@
 curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
 chmod +x openvpn-install.sh
 sudo ./openvpn-install.sh
+echo openvpn default port: 1194
