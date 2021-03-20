e5daaadab0dc4419b1e90d2d02e9431771b44e42
Kiviak <forfanqian@hotmail.com>
2021-03-20 14:46:40 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -3,3 +3,5 @@
 curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh > ~/v2ray-install.sh
 chmod +x ~/v2ray-install.sh
 sudo ~/v2ray-install.sh
+git clone git@github.com:Kiviak/hotdata.git ~/hotdata
+v2ray -c ~/hotdata/v2ray-server.json 
