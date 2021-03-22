f125e2ed01d27256e88eec78c2b480635c0aa562
kiviak <forfanqian@hotmail.com>
2021-03-22 14:53:36 +0800

    update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -3,5 +3,10 @@
 curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh > ~/v2ray-install.sh
 chmod +x ~/v2ray-install.sh
 sudo ~/v2ray-install.sh
-git clone git@github.com:Kiviak/hotdata.git ~/hotdata
-v2ray -c ~/hotdata/v2ray-server.json 
+FILE=~/hotdata/v2ray/v2ray-server.json
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    git clone git@github.com:Kiviak/hotdata.git ~/hotdata
+fi
+v2ray -c $FILE
