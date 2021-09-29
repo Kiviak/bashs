b3e1511c6b5f529e9f47779d449e0c8c2b88d48a
kiviak <forfanqian@hotmail.com>
2021-09-29 23:27:34 +0800

    let it output a summary of information

--- py/hash.py
+++ py/hash.py
@@ -4,6 +4,7 @@
 
 
 import hashlib
+import pathlib
 import sys
 
 if __name__=='__main__':
@@ -23,26 +24,41 @@ if __name__=='__main__':
         'blake2s':hashlib.blake2s,
         }
     
+    print('start:')
+    print('~'*50)
+
     data_list=[]
     for filename in file_list:
         data=[]
+        file_size=0
+        filename=pathlib.Path(filename)
+        msize=filename.stat().st_size
+        print('size:%8.3f MB  %s\n'%(msize/(1024**2),str(filename.resolve())))
+
         with open(filename,"rb") as f:
             hash_obj={}
             for key in hash_list:
                 hash_obj[key]=hash_funs[key]()
 
             # Read and update hash string value in blocks of 4K*1024*10(that is 40MB)
-            for byte_block in iter(lambda: f.read(4096*1024*10),b""):
+            block_size=4096*1024*10
+            i=0
+            for byte_block in iter(lambda: f.read(block_size),b""):
+                file_size+=block_size
+                i+=1
+                if not i%5:
+                    print('%4.2f%%'%(file_size*100/msize))
                 for key in hash_obj:
                     hash_obj[key].update(byte_block)
-            print(filename)
 
+            print('-'*50)
+            print(str(filename.resolve()),'\n')
             for key in hash_obj:
                 hex_str=hash_obj[key].hexdigest()
                 print('%-15s    %s'%(key,hex_str))
                 data.append(hex_str)
             data_list.append(data)
-            print('-'*25)
+            print('-'*50)
 
     if len(file_list)==2:
         if data_list[0]==data_list[1]:
